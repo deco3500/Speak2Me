3375694ac56e694f5075ff4865edea0fc90daa8b
george williams <george williams>
2016-10-07 08:21:24 +1000

    Fixed the positions of the next and before buttons. Added an overlay for the settings.

--- speak2me/js/general_js.js
+++ speak2me/js/general_js.js
@@ -6,3 +6,16 @@ $('#iconified').on('keyup', function() {
         input.removeClass('empty');
     }
 });
+
+
+
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
