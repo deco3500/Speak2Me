2e2a19962be542bdb72799cb4ddb4f2fbbcdb4b5
george williams <george williams>
2016-10-12 14:08:00 +1000

    Changed the 2nd browse button to the music player overlay

--- speak2me/js/general_js.js
+++ speak2me/js/general_js.js
@@ -15,7 +15,16 @@ function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 
+function openNavMusic() {
+    document.getElementById("myNavmusic").style.width = "100%";
+}
+
+
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+}
+
+function closeNavMusic() {
+    document.getElementById("myNavmusic").style.width = "0%";
 }
