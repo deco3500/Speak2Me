4715992416b1abd32a4eac8a67072f22fd5e3ab9
george williams <george williams>
2016-10-13 23:55:18 +1000

    Fixed up the side menu a bit so that it closes correctly

--- speak2me/js/webpage.js
+++ speak2me/js/webpage.js
@@ -1,17 +1,45 @@
 
 var sideup = 0;
 
-$("div .top-nav-link a").click(function(){
+$(".side-bar").click(function(){
 	if(sideup == 0){
+
+
+
 	$("#Sidenav").css("transition", "0.5s");
- 	$("#Sidenav").css("width", "300px"); 	
- 	$("body div").css("opacity","0.6");
+	$(".account").css("display", "block");
+ 		
+ 	$("body div").css("opacity","1");
  	$("body #Sidenav").css("opacity", "1");
  	sideup = 1;
     }
     else{
     	$("#Sidenav").css("transition", "0.2s");
-        $("#Sidenav").css("width", "0px");
+       
+        $(".account").css("display", "none");
+ 	    sideup = 0;
+ 	    $("body div").css("opacity","1");
+    }
+})
+
+
+
+$("#Sidenav-clear").click(function(){
+	if(sideup == 0){
+
+
+
+	$("#Sidenav").css("transition", "0.5s");
+	$(".account").css("display", "block");
+ 		
+ 	$("body div").css("opacity","1");
+ 	$("body #Sidenav").css("opacity", "1");
+ 	sideup = 1;
+    }
+    else{
+    	$("#Sidenav").css("transition", "0.2s");
+       
+        $(".account").css("display", "none");
  	    sideup = 0;
  	    $("body div").css("opacity","1");
     }
