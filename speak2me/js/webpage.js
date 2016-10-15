baee80bc18d555c9693011f8d78621b01733914f
diego robles <diego_robles99@hotmail.com>
2016-10-16 01:48:39 +1000

    Polished account menu for reputation concept and added tag selection menu

--- speak2me/js/webpage.js
+++ speak2me/js/webpage.js
@@ -45,10 +45,47 @@ $("#Sidenav-clear").click(function(){
     }
 })
 
+$(".edit-button").click(function(){
+    $(".tags-menu").css("visibility", "visible");
+    $(".tags-menu").css("opacity", "1");
+    $(".overlay-wrapper-tags").css("display", "block");
+});
+
+$(".overlay-wrapper-tags").click(function(){
+    $(".tags-menu").css("visibility", "hidden");
+    $(".tags-menu").css("opacity", "0");
+    $(".overlay-wrapper-tags").css("display", "none");
+})
+
+function closeTagMenu(){
+    $(".tags-menu").css("visibility", "hidden");
+    $(".tags-menu").css("opacity", "0");
+    $(".overlay-wrapper-tags").css("display", "none");
+}
+
+$(".tags-menu .tag-image-text").click(function(){
+    if($(this).parent().hasClass("tag-selected")){
+        $(this).parent().removeClass("tag-selected");
+    } 
+    else{
+        $(this).parent().addClass("tag-selected");
+    }
+      
+});
+
+$(".tags-menu button").click(function(){
+    $(".tags-menu .column").each(function(){
+        if($(this).hasClass("tag-selected")){
+            var tag = $(this).text();
+            $(".account-details p:contains('Preferred tags') .tag:last-of-type .fa").parent().after(
+                $("<span class='tag'>"+ tag +"<span class='fa fa-times-circle created-tag'></span></span>"));
+        }
+    });
+    closeTagMenu();
+
+});
 
 
-
-
-
-
-
+$(document.body).on("click", ".fa-times-circle", function(){
+    $(this).parent().remove();
+})
